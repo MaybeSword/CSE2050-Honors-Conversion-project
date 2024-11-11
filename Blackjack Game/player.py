d76da89a1697cc67dc4f77f06d1e4db20b50cd69
Chris Rauch <crauch333@gmail.com>
2024-11-10 19:28:05 -0500

    basic changes

--- Blackjack Game/player.py
+++ Blackjack Game/player.py
@@ -21,6 +21,9 @@ class Player:
         if hand.calculate_value() > 21:
             self.busted = True
 
+    def basic_strategy_play(self):
+        pass
+
     def count_cards_play(self):
         # 2 - 6  is plus1, 10 - A, is minus1, 7-9 neutral
         pass
