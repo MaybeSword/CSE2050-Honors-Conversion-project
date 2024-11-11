582eb6daa282c1f06e900ecd25d5632d3de25f6d
Chris Rauch <crauch333@gmail.com>
2024-11-10 22:07:57 -0500

    slight clerical change

--- Blackjack Game/card.py
+++ Blackjack Game/card.py
@@ -5,13 +5,13 @@ class Card:
         self.suit = suit
         self.faceup = faceup
 
-    def card_value(card):
+    def card_value(self):
         val = 0
         L_nums = [2, 3, 4, 5, 6, 7, 8, 9, 10]
         face_cards = ['J', 'Q', 'K']
-        if card.rank in L_nums:
-            val = card.rank
-        elif card.rank in face_cards:
+        if self.rank in L_nums:
+            val = self.rank
+        elif self.rank in face_cards:
             val = 10
         else: 
             val += 11
