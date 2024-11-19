c13a4864d70bdb24b06a11f02e40c86b3f31ca36
Sword <67487902+MaybeSword@users.noreply.github.com>
2024-11-18 19:30:56 -0500

    modified:   Blackjack Game/blackjack_gui.py

--- Blackjack Game/blackjack_gui.py
+++ Blackjack Game/blackjack_gui.py
@@ -8,6 +8,7 @@ class BlackjackGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Blackjack")
+        self.root.geometry("800x600")
         
         self.controller = BlackjackController(self)
 
